f2f3d7170c7ff4870cad5f8838f451eaebc5ca90
redweller <redweller@gmail.com>
2025-01-11 00:50:10 +0300

    add node uninstall and fresh install options

--- node/uninstall.js
+++ node/uninstall.js
@@ -2,22 +2,18 @@ const fs = require ('fs');
 
 const mw = "path to your Morrowind installation";
 const id = "Simple Progress Bars";
-const freshInstall = false;
 
 const path = mw.slice(-1) === "\\"
 	? `${mw}Data Files\\MWSE\\`
 	: `${mw}\\Data Files\\MWSE\\`;
 
-const src = `MWSE\\mods\\${id}`;
-
 const mods = `${path}\\mods\\${id}`;
 const conf = `${path}\\config\\${id}.json`;
 
 if (fs.existsSync(path)) {
-	console.log("Morrowind directory found. Installing mod");
+	console.log("Morrowind directory found. Uninstalling mod");
 	fs.rmSync(mods, {recursive: true, force: true});
-	fs.cpSync(src, mods, {recursive: true});
-	freshInstall && fs.rmSync(conf, {force: true});
+	fs.rmSync(conf, {force: true});
 } else {
 	console.log("Morrowind directory not found");
 }
